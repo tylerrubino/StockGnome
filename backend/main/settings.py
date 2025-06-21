5d7673bd27898192c6e7d177d0580813a2c5b22b
Tyler Rubino <tylerrubi9@gmail.com>
2025-06-21 18:56:10 -0400

    cors, registration ui and call to backend, login component made, logo updated

--- backend/main/settings.py
+++ backend/main/settings.py
@@ -41,16 +41,18 @@ INSTALLED_APPS = [
     'rest_framework',
     'accounts',
     'api',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware'
 ]
 
 ROOT_URLCONF = 'main.urls'
@@ -124,3 +126,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173',
+]
---
+++ frontend/src/components/Register.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+
+const Register = () => {
+  const [username, setUsername] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleRegistration = async (e) => {
+    e.preventDefault();
+    
+    const userData = {
+      username, email, password
+    }
+
+    try {
+      const response = await axios.post('http://127.0.0.1:8000/api/v1/register/', userData);
+      console.log('response.data===>', response.data);
+      console.log('registration success!')
+    } catch(error) {
+      console.error('Registration error: ', error.response.data);
+    }
+  }
+
+  return (
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-md-6 bg-light-dark p-5 rounded">
+            <h3 className='text-light text-center mb-5'>Create an account</h3>
+            <form onSubmit={handleRegistration}>
+              <input type="text" className='form-control mb-3' placeholder='Username' value={username} onChange={(e) => setUsername(e.target.value)}/>
+              <input type="email" className='form-control mb-3' placeholder='Email' value={email} onChange={(e) => setEmail(e.target.value)}/>
+              <input type="password" className='form-control mb-5' placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)}/>
+              <button type='sbmit' className='btn btn-info d-block mx-auto'>Register</button>
+            </form>
+          </div>
+        </div>
+      </div>
+  )
+}
+
+export default Register 
