b14702cf55ae37d5668e5a77c98c7bdc0b0719d7
Tyler Rubino <tylerrubi9@gmail.com>
2025-08-18 21:00:17 -0400

    prepping for deployment

--- backend/main/urls.py
+++ backend/main/urls.py
@@ -13,5 +13,5 @@ urlpatterns = [
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += [
-    static(settins.STATIC_URL, document_root=settings.STATIC_ROOT),
+    static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
 ]
